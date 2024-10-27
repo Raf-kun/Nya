09f36d073f66f1f1beede0977fc8b68e7af5250c
Raf-kun <14katosik03@gmail.com>
2024-10-27 18:10:36 +0000

    I have edit calc

--- calc.py
+++ calc.py
@@ -1,4 +1,18 @@
-number1 = (input("Введите сумму которую вы хотите дать в фонд Кати "))
-number2 = (input("Введите число на сколько вы меня любите "))
-result = number1 + number2
-print(result)
+number1 = int(input("Введите сумму которую вы хотите дать в фонд Кати "))
+sign = input("Введите символ: ")
+number2 = int(input("Введите число на сколько вы меня любите "))
+
+if sign == "+":
+    print(number1 + number2)
+elif sign == "-":
+    print(number1 - number2)
+    if number1 > number2:
+        print(number1 - number2)
+    else:
+        print(number2 - number1)
+elif sign == "/" and number2 != 0:
+    print(number1 / number2)
+elif sign == "*":
+    print(number1 * number2)
+else:
+    print("Я не знаю, скорее всего вы бот)")
